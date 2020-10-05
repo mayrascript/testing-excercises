6928f21b51d59af3ed950e25b923ebc0746580d0
Mayra <mayrarodriguez1709@gmail.com>
2020-10-05 08:35:09 -0500

    feat: some funtions to test

--- index.js
+++ index.js
@@ -1,4 +1,46 @@
 function sum(a, b) {
     return a + b;
 }
+
+const momIsMadRandomAnswer = () => (Math.floor(Math.random() * (5 - 0)) + 0) > 3;
+
+let array = [7,8,1,52];
+
+function reverseArray(arr) {
+    let count = arr.length;
+    let reversed = [];
+    do{
+        const value = arr[count - 1];
+        reversed.push(value);
+        count -= 1;
+    } while(count > 0);
+
+    return reversed;
+}
+
+let promise = new Promise((resolve, reject) => {
+    //reject(err);
+    resolve(3);
+})
+
+function multiplication() {
+    return new Promise((resolve, reject) => {
+        resolve([1,2,3,4]);
+    });
+}
+
+
+function birthdayGift() {
+    return new Promise((resolve, reject) => {
+        console.log('hola')
+        const momIsMad = momIsMadRandomAnswer();
+        if(momIsMad) {
+            const err = new Error(
+                `Mi madre decidió no comprarme un nuevo teléfono`);
+            reject(err);
+        }
+
+        resolve(`Mi mamá está feliz!, me compró un nuevo teléfono`);
+    });
+}
 module.exports = sum;
